b0d2dd74a0e38b6da6e683db9756e3cd7c797fd0
bas <tima.bashirov92@gmail.com>
2025-09-17 21:50:29 +0300

    feat: реализация пакетов actioninfo, daysteps и trainings

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,13 +1,46 @@
 package daysteps
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/mrbastim/go-5-sprint-final/internal/personaldata"
+	"github.com/mrbastim/go-5-sprint-final/internal/spentenergy"
+)
+
 type DaySteps struct {
 	// TODO: добавить поля
+	Steps    int
+	Duration time.Duration
+	personaldata.Personal
 }
 
 func (ds *DaySteps) Parse(datastring string) (err error) {
 	// TODO: реализовать функцию
+	data := strings.Split(datastring, ",")
+	if len(data) != 2 {
+		return fmt.Errorf("неверное количество данных")
+	}
+	ds.Steps, err = strconv.Atoi(data[0])
+	if err != nil {
+		return fmt.Errorf("неверный формат количества шагов: %v", err)
+	}
+	ds.Duration, err = time.ParseDuration(data[1])
+	if err != nil {
+		return fmt.Errorf("неверный формат длительности: %v", err)
+	}
+	return nil
 }
 
 func (ds DaySteps) ActionInfo() (string, error) {
 	// TODO: реализовать функцию
+	distance := spentenergy.Distance(ds.Steps, ds.Personal.Height)
+	callories, err := spentenergy.WalkingSpentCalories(ds.Steps, ds.Personal.Weight, ds.Personal.Height, ds.Duration)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.",
+		ds.Steps, distance, callories), nil
 }
--- internal/trainings/trainings.go
+++ internal/trainings/trainings.go
@@ -1,13 +1,63 @@
 package trainings
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/mrbastim/go-5-sprint-final/internal/personaldata"
+	"github.com/mrbastim/go-5-sprint-final/internal/spentenergy"
+)
+
 type Training struct {
 	// TODO: добавить поля
+	Steps        int
+	TrainingType string
+	Duration     time.Duration
+	personaldata.Personal
 }
 
 func (t *Training) Parse(datastring string) (err error) {
 	// TODO: реализовать функцию
+	data := strings.Split(datastring, ",")
+	if len(data) != 3 {
+		return fmt.Errorf("неверное количество данных")
+	}
+	t.Steps, err = strconv.Atoi(data[0])
+	if err != nil {
+		return fmt.Errorf("неверный формат количества шагов: %v", err)
+	}
+	t.TrainingType = data[1]
+
+	t.Duration, err = time.ParseDuration(data[2])
+	if err != nil {
+		return fmt.Errorf("неверный формат длительности тренировки: %v", err)
+	}
+	return nil
 }
 
 func (t Training) ActionInfo() (string, error) {
 	// TODO: реализовать функцию
+	distance := spentenergy.Distance(t.Steps, t.Personal.Height)
+	meanSpeed := spentenergy.MeanSpeed(t.Steps, t.Personal.Height, t.Duration)
+
+	switch t.TrainingType {
+	case "Бег":
+		callories, err := spentenergy.RunningSpentCalories(t.Steps, t.Personal.Weight, t.Personal.Height, t.Duration)
+		if err != nil {
+			return "", err
+		}
+		return fmt.Sprintf("Тип тренировки: Бег\nДлительность: %v\nДистанция: %v\nСкорость: %v\nСожгли калорий: %v",
+			t.Duration, distance, meanSpeed, callories), nil
+	case "Ходьба":
+		callories, err := spentenergy.WalkingSpentCalories(t.Steps, t.Personal.Weight, t.Personal.Height, t.Duration)
+		if err != nil {
+			return "", err
+		}
+		return fmt.Sprintf("Тип тренировки: Ходьба\nДлительность: %v\nДистанция: %v\nСкорость: %v\nСожгли калорий: %v",
+			t.Duration, distance, meanSpeed, callories), nil
+	default:
+		return "", fmt.Errorf("неизвестный тип тренировки")
+	}
 }
