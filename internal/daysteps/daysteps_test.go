73cfd9eda7e0a690de9d6cdea55c992b9e8203e1
bas <tima.bashirov92@gmail.com>
2025-09-17 14:02:51 +0300

    feat: реализация пакетов personaldata и spentenergy

--- internal/daysteps/daysteps_test.go
+++ internal/daysteps/daysteps_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/Yandex-Practicum/tracker/internal/personaldata"
+	"github.com/mrbastim/go-5-sprint-final/internal/personaldata"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
--- internal/spentenergy/spentenergy.go
+++ internal/spentenergy/spentenergy.go
@@ -1,6 +1,7 @@
 package spentenergy
 
 import (
+	"fmt"
 	"time"
 )
 
@@ -14,16 +15,38 @@ const (
 
 func WalkingSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
 	// TODO: реализовать функцию
+	if steps <= 0 || weight <= 0 || height <= 0 || duration <= 0 {
+		return 0, fmt.Errorf("некорректные данные")
+	}
+	mSpeed := MeanSpeed(steps, height, duration)
+	durMin := duration.Minutes()
+	callories := (weight * mSpeed * durMin) / minInH
+	return callories * walkingCaloriesCoefficient, nil
 }
 
 func RunningSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
 	// TODO: реализовать функцию
+	if steps <= 0 || weight <= 0 || height <= 0 || duration <= 0 {
+		return 0, fmt.Errorf("некорректные данные")
+	}
+	mSpeed := MeanSpeed(steps, height, duration)
+	durMin := duration.Minutes()
+	return (weight * mSpeed * durMin) / minInH, nil
 }
 
 func MeanSpeed(steps int, height float64, duration time.Duration) float64 {
 	// TODO: реализовать функцию
+	if steps < 0 || duration <= 0 {
+		return 0
+	}
+
+	distance := Distance(steps, height)
+	return distance / duration.Hours()
 }
 
 func Distance(steps int, height float64) float64 {
 	// TODO: реализовать функцию
+	stepLength := height * stepLengthCoefficient
+	distance := steps * int(stepLength) / mInKm
+	return float64(distance)
 }
